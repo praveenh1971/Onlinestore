adb1ac1f2949aaa13dba0e33e1e164edf66ba976
sanjiv <sanjiv@Sanju-Lenova>
2021-08-17 22:27:11 -0400

    updates

--- ProductService/lib/src/main/java/com/onlinestore/products/ProductServiceApplication.java
+++ ProductService/lib/src/main/java/com/onlinestore/products/ProductServiceApplication.java
@@ -10,7 +10,7 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 			
 		Vertx vrts = Vertx.vertx();
-		vrts.deployVerticle(new ProductServiceTest(3001));
+		vrts.deployVerticle(new ProductService(3001));
 		while(true);
 	}
 	
